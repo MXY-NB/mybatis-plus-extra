01759d9cb0d99712f39140926167a72c0cc83330
莫晓雨 <849280429@qq.com>
2023-12-20 09:52:19 +0000

    删除文件 src

--- src/main/resources/script/startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set JAVA_OPTIONS=-Xms512M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError
-for /f "usebackq" %%i in (`dir kx-game*REL.jar /b`) do set PRJ_JAR=%%i
-
-xjar java %JAVA_OPTIONS%  -jar  %PRJ_JAR% --spring.profiles.active=pro
-
-pause
